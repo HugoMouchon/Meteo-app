a0efd58ad010c127f1e4e204b1b512cb0ffa3de1
Hugo Mouchon <hugomouchonpro@gmail.com>
2023-06-19 09:58:35 +0200

    Installation de la librairie Axios, puis développement de la requete API Meteo Weather stocké et mise à jours dans le state weather sellon les cooordonnées du GPS de l'utilisateur

--- pages/Home/Home.jsx
+++ pages/Home/Home.jsx
@@ -3,16 +3,31 @@ import { s } from "./Home.style";
 import { useEffect, useState } from "react";
 import * as NavigationBar from 'expo-navigation-bar';
 import { requestForegroundPermissionsAsync, getCurrentPositionAsync } from "expo-location";
+import { MeteoAPI } from "../../api/meteo";
 
 export function Home() {
 
     // Etat qui permet de stocker les coordonnées du téléphone
     const [coords, setCoords] = useState();
+    // Etat qui permet de stocker les doonnées de l'API via les coordonnées de l'utilisateur
+    const [weather, setWeather] = useState();
+
+    // Permet de changer la couleur de la NavigationBar d'Android
+    useEffect(() => {
+        NavigationBar.setBackgroundColorAsync("#000040");
+    }, []);
 
     // Permet de récuperer les positions GPS une seule fois lors du chargemment de l'application
     useEffect(() => {
         getUserCoords();
     }, []);
+
+    // Permet de faire appel à la fonction fetchWeather chaque fois que les coordonnées changent.
+    useEffect(() => {
+        if (coords) {
+            fetchWeather(coords)
+        }
+    }, [coords]);
 
     // Fonction qui permet de demander à l'utilisateur de partager ou non sa position GPS.
     // Si OUI ====> on récupère ses positions est on les sauvegardes via le setCoords prévu à cet effet.
@@ -29,12 +44,17 @@ export function Home() {
             setCoords({ lat: "48.85", lng: "2.35" });
         }
     }
-    console.log(coords);
 
-    // Permet de changer la couleur de la NavigationBar d'Android
-    useEffect(() => {
-        NavigationBar.setBackgroundColorAsync("#000040");
-    }, []);
+    // Fonction asynchrone (promesse, attente) qui permet de récuperer les données de l'API selon les coordonnées du téléphone
+    // Il stock en suite les données attendu dans le state "weather".
+    async function fetchWeather(coordinates) {
+        const weatherResponse = await MeteoAPI.fetchWeatherFromCoords(coordinates);
+        setWeather(weatherResponse);
+    }
+
+    console.log(weather);
+
+
 
     return (
         <>
